c200acff6893c022db8f0cbbbbbf242a255e3b38
randleignacio <randle29@ufl.edu>
2017-08-29 10:35:32 -0400

    added most functionality

--- listingController.js
+++ listingController.js
@@ -1,14 +1,29 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+
+
+    $scope.addListing = function(obj) {
+      $scope.listings.push(obj);
+      // $scope.myForm.code.$setPristine();
+      // $scope.myForm.code.$setPristine(true);
+      // $scope.myField = '';
+    	//$scope.headoffice='';
+
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
+
   }
 ]);
